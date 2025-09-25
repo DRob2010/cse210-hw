e47ea7e16676e6bbceb2d6e1b43916e5f90e0c17
cookiecatz21 <rob21092@byui.edu>
2025-09-25 10:31:29 -0600

    prep#5 Function Fix

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -14,39 +14,39 @@ class Program
         int squared_favorite_number = SquareNumber(favorite_number);
         DisplayResult(typed_name, squared_favorite_number, birth_year);
 
-        void DisplayWelcome()
+        static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the program!");
         }
 
-        string PromptUserName()
+        static string PromptUserName()
         {
             Console.Write("Please enter your name: ");
             string typed_name = Console.ReadLine();
             return typed_name;
         }
 
-        int PromptUserNumber()
+        static int PromptUserNumber()
         {
             Console.Write("Please enter your favorite number: ");
             string string_number = Console.ReadLine();
             int favorite_number = int.Parse(string_number);
             return favorite_number;
         }
-        void PromtUserBirthYear(out int birth_year)
+        static void PromtUserBirthYear(out int birth_year)
         {
             Console.Write("Please enter the year you were born: ");
             string string_year = Console.ReadLine();
             birth_year = int.Parse(string_year);
         }
 
-        int SquareNumber(int number)
+        static int SquareNumber(int number)
         {
             int squared_favorite_number = number * number;
             return squared_favorite_number;
         }
 
-        void DisplayResult(string typed_name, int squared_favorite_number, int birth_year)
+        static void DisplayResult(string typed_name, int squared_favorite_number, int birth_year)
         {
             Console.WriteLine($"{typed_name}, the square of your number is {squared_favorite_number}");
             int how_old = 2025 - birth_year;
