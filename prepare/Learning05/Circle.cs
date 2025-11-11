c91e9d609eccac52dda89d46dfbfe29ce8f1ba91
cookiecatz21 <rob21092@byui.edu>
2025-11-11 10:49:14 -0700

    Learning 5 Shapes

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(double radius, string color) : base(color)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        double area = _radius * _radius * Math.PI;
+        return area;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square(4, "green");
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle(5, 6, "blue");
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle(2, "red");
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Your {shape} has an area of {area} and is the color {color}.");
+        }
     }
 }
